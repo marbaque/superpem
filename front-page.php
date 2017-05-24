6b00caccafa1e7a828f91d74aee4166d53d450b8
Mario Badilla <mario.badilla@gmail.com>
2017-05-24 15:01:00 -0600

    pem-24-5

--- front-page.php
+++ front-page.php
@@ -13,28 +13,36 @@ get_header();
     <main id="main" class="site-main" role="main">
         <?php while (have_posts()) : the_post(); ?>
 
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="entry-header">
-                    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-                </header><!-- .entry-header -->
+            <article id="post-<?php the_ID(); ?>" class="flex-container">
 
-                <div class="entry-content">
-                    <?php
-                    the_content();
-                    ?>
-                </div><!-- .entry-content -->
+                <div class="col uno">
 
-                    
+
+                    <div class="entry-content">
+                        <?php
+                        the_content();
+                        ?>
+                    </div><!-- .entry-content -->
+                </div>
+
+                
+                <?php if (has_post_thumbnail($post->ID)): ?>
+                <div class="col dos">
+                <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), "single-post-thumbnail"); ?>
+                </div>
+                <?php endif; ?>
+                
+
+
             </article><!-- #post-## -->
 
 
 
-            <?php endwhile; // End of the loop.
-            ?>
+        <?php endwhile; // End of the loop.
+        ?>
 
     </main><!-- #main -->
 </div><!-- #primary -->
 
-        <?php
-        get_footer();
-        
+<?php
+get_footer();
